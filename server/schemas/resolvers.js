ff901f288bb5123cf821078fb815973461b994d6
Austin Huffman <austinhuffman0@gmail.com>
2021-08-24 02:07:26 -0400

    addUser and login paths created for Resolvers.  Unsure about use of spread operator on args in addUser Unsure about or operator in the findOne method under login keep eye on these if any errors occur

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,37 @@
 const {Book,User} = require('../models');
+const {AuthenticationError} = require ('apollo-server-express');
+const {signToken} = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        //TBD
+
     },
     Mutation: {
         //LOOK AT user-controller.js as reference. 
+
+        //Adds an individual user and signs a token to that new user
+        addUser: async(parent, args) => {
+            const user = await User.create(...args);
+            const token = signToken(user);
+            return {token,user};
+        },
+        login: async (parent, args) => {
+
+            const user = await User.findOne({ $or: [{username: args.username}, {email: args.email}] }); // IDK IF THIS WILL WORK
+            if (!user){
+                throw new AuthenticationError('There are No Users found with that Username or Email');
+            }
+
+            const correctPw = await user.isCorrectPassword(args.password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Wrong Password!');
+            }
+
+            const token = signToken(user);
+            return {token,user};
+
+        }
     }
 }
 
