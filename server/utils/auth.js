3c8f6806cdf1f87b3d87e4313f276ea4cfee55cd
Austin Huffman <austinhuffman0@gmail.com>
2021-08-24 23:04:15 -0400

    stuff

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization || req.body.token;
 
@@ -28,7 +28,7 @@ module.exports = {
       console.log('Invalid token');
       //return res.status(400).json({ message: 'invalid token!' });
     }
-
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
