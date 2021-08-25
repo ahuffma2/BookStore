fd989d5822af9b069c44a6a4787a040892b20087
Austin Huffman <austinhuffman0@gmail.com>
2021-08-24 21:54:52 -0400

    fixed savedBooks

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
-import {ADD_USER } from '../utils/mutations';
+import { ADD_USER } from '../utils/mutations';
 //import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -38,7 +38,7 @@ const SignupForm = () => {
         variables: { ...userFormData},
       });
       Auth.login(data.addUser.token);
-      
+
       // const response = await createUser(userFormData);
 
       // if (!response.ok) {
